7608bb96879156c128f3a9317a5a8176d94a02de
Yuki Kishimoto <yukikishimoto@proton.me>
2022-09-11 23:20:32 +0200

    Refactoring db

--- src/db.rs
+++ src/db.rs
@@ -3,9 +3,11 @@
 
 use std::collections::HashMap;
 use std::path::Path;
+use std::sync::Arc;
 
-use bpns_rocksdb::{Error, Store};
+use bpns_rocksdb::{BoundColumnFamily, Error, Store};
 
+use crate::primitives::Target;
 use crate::util;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -16,6 +18,7 @@ pub struct Session {
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Notification {
+    pub target: Target,
     pub plain_text: String,
     pub html: String,
 }
@@ -38,6 +41,22 @@ impl DBStore {
         })
     }
 
+    fn session_cf(&self) -> Arc<BoundColumnFamily> {
+        self.db.cf_handle(SESSION_CF)
+    }
+
+    fn subscription_cf(&self) -> Arc<BoundColumnFamily> {
+        self.db.cf_handle(SUBSCRIPTION_CF)
+    }
+
+    fn network_cf(&self) -> Arc<BoundColumnFamily> {
+        self.db.cf_handle(NETWORK_CF)
+    }
+
+    fn notification_cf(&self) -> Arc<BoundColumnFamily> {
+        self.db.cf_handle(NOTIFICATION_CF)
+    }
+
     pub fn create_session(
         &self,
         user_id: &str,
@@ -49,65 +68,82 @@ impl DBStore {
             device_id: device_id.into(),
         };
 
-        self.db
-            .put_serialized(self.db.cf_handle(SESSION_CF), user_id, &value)
+        self.db.put_serialized(self.session_cf(), user_id, &value)
     }
 
     pub fn session_exist(&self, user_id: &str) -> bool {
-        self.db.get(self.db.cf_handle(SESSION_CF), user_id).is_ok()
+        self.db.get(self.session_cf(), user_id).is_ok()
     }
 
     pub fn get_session(&self, user_id: &str) -> Result<Session, Error> {
-        self.db
-            .get_deserialized(self.db.cf_handle(SESSION_CF), user_id)
+        self.db.get_deserialized(self.session_cf(), user_id)
     }
 
     pub fn create_subscription(&self, room_id: &str) -> Result<(), Error> {
-        self.db
-            .put(self.db.cf_handle(SUBSCRIPTION_CF), room_id, "true")
+        self.db.put(self.subscription_cf(), room_id, "true")
     }
 
     pub fn subscription_exist(&self, room_id: &str) -> bool {
-        self.db
-            .get(self.db.cf_handle(SUBSCRIPTION_CF), room_id)
-            .is_ok()
+        self.db.get(self.subscription_cf(), room_id).is_ok()
     }
 
     pub fn delete_subscription(&self, room_id: &str) -> Result<(), Error> {
-        self.db.delete(self.db.cf_handle(SUBSCRIPTION_CF), room_id)
+        self.db.delete(self.subscription_cf(), room_id)
     }
 
     pub fn get_subscriptions(&self) -> Result<Vec<String>, Error> {
         let collection = self
             .db
-            .iterator_str_serialized::<bool>(self.db.cf_handle(SUBSCRIPTION_CF))?;
+            .iterator_str_serialized::<bool>(self.subscription_cf())?;
 
         Ok(collection.keys().cloned().collect())
     }
 
-    pub fn create_notification(&self, plain_text: &str, html: &str) -> Result<(), Error> {
-        let key: &str = &util::sha512(format!("{}:{}", plain_text, html))[..32];
+    pub fn create_notification(
+        &self,
+        target: Target,
+        plain_text: &str,
+        html: &str,
+    ) -> Result<(), Error> {
+        let key: &str = &util::sha512(format!("{}:{}:{}", target, plain_text, html))[..32];
         let value: Notification = Notification {
+            target,
             plain_text: plain_text.into(),
             html: html.into(),
         };
 
-        self.db
-            .put_serialized(self.db.cf_handle(NOTIFICATION_CF), key, &value)
+        self.db.put_serialized(self.notification_cf(), key, &value)
     }
 
-    pub fn get_notifications(&self) -> Result<HashMap<String, Notification>, Error> {
+    /* pub fn get_all_notifications(&self) -> Result<HashMap<String, Notification>, Error> {
         self.db
-            .iterator_str_serialized::<Notification>(self.db.cf_handle(NOTIFICATION_CF))
+            .iterator_str_serialized::<Notification>(self.notification_cf())
+    } */
+
+    pub fn get_notifications_by_target(
+        &self,
+        target: Target,
+    ) -> Result<HashMap<String, Notification>, Error> {
+        let mut notification: HashMap<String, Notification> = HashMap::new();
+        let collection = self
+            .db
+            .iterator_str_serialized::<Notification>(self.notification_cf())?;
+
+        collection.into_iter().for_each(|(key, value)| {
+            if value.target == target {
+                notification.insert(key, value);
+            }
+        });
+
+        Ok(notification)
     }
 
     pub fn delete_notification(&self, notification_id: &str) -> Result<(), Error> {
-        self.db
-            .delete(self.db.cf_handle(NOTIFICATION_CF), notification_id)
+        self.db.delete(self.notification_cf(), notification_id)
     }
 
     pub fn get_last_processed_block(&self) -> Result<u64, Error> {
-        let cf = self.db.cf_handle(NETWORK_CF);
+        let cf = self.network_cf();
         match self.db.get(cf, "last_processed_block") {
             Ok(result) => match util::bytes_to_number::<u64>(result) {
                 Some(num) => Ok(num),
@@ -118,14 +154,15 @@ impl DBStore {
     }
 
     pub fn set_last_processed_block(&self, block_height: u64) -> Result<(), Error> {
-        let cf = self.db.cf_handle(NETWORK_CF);
-        self.db
-            .put(cf, "last_processed_block", block_height.to_string())
+        self.db.put(
+            self.network_cf(),
+            "last_processed_block",
+            block_height.to_string(),
+        )
     }
 
     pub fn get_last_difficulty(&self) -> Result<f64, Error> {
-        let cf = self.db.cf_handle(NETWORK_CF);
-        match self.db.get(cf, "last_difficulty") {
+        match self.db.get(self.network_cf(), "last_difficulty") {
             Ok(result) => match util::bytes_to_number::<f64>(result) {
                 Some(num) => Ok(num),
                 None => Err(Error::FailedToDeserialize),
@@ -135,13 +172,12 @@ impl DBStore {
     }
 
     pub fn set_last_difficculty(&self, difficulty: f64) -> Result<(), Error> {
-        let cf = self.db.cf_handle(NETWORK_CF);
-        self.db.put(cf, "last_difficulty", difficulty.to_string())
+        self.db
+            .put(self.network_cf(), "last_difficulty", difficulty.to_string())
     }
 
     pub fn get_last_supply(&self) -> Result<f64, Error> {
-        let cf = self.db.cf_handle(NETWORK_CF);
-        match self.db.get(cf, "last_supply") {
+        match self.db.get(self.network_cf(), "last_supply") {
             Ok(result) => match util::bytes_to_number::<f64>(result) {
                 Some(num) => Ok(num),
                 None => Err(Error::FailedToDeserialize),
@@ -151,13 +187,12 @@ impl DBStore {
     }
 
     pub fn set_last_supply(&self, hashrate: f64) -> Result<(), Error> {
-        let cf = self.db.cf_handle(NETWORK_CF);
-        self.db.put(cf, "last_supply", hashrate.to_string())
+        self.db
+            .put(self.network_cf(), "last_supply", hashrate.to_string())
     }
 
     pub fn get_last_hashrate_ath(&self) -> Result<f64, Error> {
-        let cf = self.db.cf_handle(NETWORK_CF);
-        match self.db.get(cf, "last_hashrate_ath") {
+        match self.db.get(self.network_cf(), "last_hashrate_ath") {
             Ok(result) => match util::bytes_to_number::<f64>(result) {
                 Some(num) => Ok(num),
                 None => Err(Error::FailedToDeserialize),
@@ -167,8 +202,8 @@ impl DBStore {
     }
 
     pub fn set_last_hashrate_ath(&self, hashrate: f64) -> Result<(), Error> {
-        let cf = self.db.cf_handle(NETWORK_CF);
-        self.db.put(cf, "last_hashrate_ath", hashrate.to_string())
+        self.db
+            .put(self.network_cf(), "last_hashrate_ath", hashrate.to_string())
     }
 }
 
